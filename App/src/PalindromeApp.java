3cd81ac89c1b1dcc3f2ef1732b0684f86bf6bcad
WasimMdRahman <rahmanwasim67@gmail.com>
2026-03-16 14:18:45 +0530

    [Wasim] added uc1

---
+++ App/src/PalindromeApp.java
@@ -0,0 +1,8 @@
+ class PalindromeApp {
+    public static void main(String[] args) {
+        System.out.println("welcome to palindrome checker app");
+        System.out.println("version 1.0");
+        System.out.println("system initialised successfully");
+
+    }
+}
