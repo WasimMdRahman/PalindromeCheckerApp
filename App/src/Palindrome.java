bb0f87f8a21605700f8a2656ea1b033fe46362bc
WasimMdRahman <rahmanwasim67@gmail.com>
2026-03-16 15:14:35 +0530

    UC 12

--- App/src/Palindrome.java
+++ App/src/Palindrome.java
@@ -1,83 +1,39 @@
 public class Main {
     public static void main(String[] args) {
 
-
-        class StackStrategy implements PalindromeStrategy {
-
-            public boolean isPalindrome(String text) {
-
-                Stack<Character> stack = new Stack<>();
-
-                for (char c : text.toCharArray()) {
-                    stack.push(c);
-                }
-
-                for (char c : text.toCharArray()) {
-                    if (c != stack.pop()) {
-                        return false;
-                    }
-                }
-
-                return true;
-            }
-        }
-
-// Deque Strategy Implementation
-        class DequeStrategy implements PalindromeStrategy {
-
-            public boolean isPalindrome(String text) {
-
-                Deque<Character> deque = new LinkedList<>();
-
-                for (char c : text.toCharArray()) {
-                    deque.addLast(c);
-                }
-
-                while (deque.size() > 1) {
-                    if (deque.removeFirst() != deque.removeLast()) {
-                        return false;
-                    }
-                }
-
-                return true;
-            }
-        }
-
-// Context Class
         class PalindromeChecker {
 
-            private PalindromeStrategy strategy;
+            // Method to check palindrome
+            public boolean checkPalindrome(String text) {
 
-            // Inject strategy at runtime
-            public PalindromeChecker(PalindromeStrategy strategy) {
-                this.strategy = strategy;
-            }
+                String reversed = "";
 
-            public boolean check(String text) {
-                return strategy.isPalindrome(text);
+                // Reverse the string
+                for (int i = text.length() - 1; i >= 0; i--) {
+                    reversed = reversed + text.charAt(i);
+                }
+
+                // Compare strings
+                return text.equals(reversed);
             }
         }
 
-// Main class
-        public class StrategyPalindromeApp {
+        public class PalindromeApp {
 
             public static void main(String[] args) {
 
-                String text = "madam";
+                String word = "madam";
 
-                // Choose strategy dynamically
-                PalindromeStrategy strategy = new StackStrategy();
-                // PalindromeStrategy strategy = new DequeStrategy();
+                // Create object of PalindromeChecker
+                PalindromeChecker checker = new PalindromeChecker();
 
-                PalindromeChecker checker = new PalindromeChecker(strategy);
-
-                if (checker.check(text)) {
-                    System.out.println(text + " is a Palindrome");
+                // Call method
+                if (checker.checkPalindrome(word)) {
+                    System.out.println(word + " is a Palindrome");
                 } else {
-                    System.out.println(text + " is not a Palindrome");
+                    System.out.println(word + " is not a Palindrome");
                 }
             }
         }
-
     }
 }
