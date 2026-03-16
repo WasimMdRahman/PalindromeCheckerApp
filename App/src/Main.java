133b7aa411c2fe92e9438d8ed52861e5a9151b50
WasimMdRahman <rahmanwasim67@gmail.com>
2026-03-16 15:08:24 +0530

    UC 10

--- App/src/Main.java
+++ App/src/Main.java
@@ -2,14 +2,30 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        class PalindromeIgnoreCaseSpace {
+
+            public static void main(String[] args) {
+
+                String text = "Madam In Eden Im Adam";
+
+                // Normalize the string
+                String normalized = text.replaceAll("\\s+", "").toLowerCase();
+
+                // Reverse the normalized string
+                String reversed = "";
+                for (int i = normalized.length() - 1; i >= 0; i--) {
+                    reversed = reversed + normalized.charAt(i);
+                }
+
+                // Compare strings
+                if (normalized.equals(reversed)) {
+                    System.out.println(text + " is a Palindrome (ignoring spaces and case)");
+                } else {
+                    System.out.println(text + " is not a Palindrome");
+                }
+            }
         }
+
     }
 }
